5b606ec4644e651b6352429dbe64fefa18597e5c
Andrea Censi <AndreaCensi@users.noreply.github.com>
2023-07-07 15:14:14 +0200

    misc

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,3 +5,8 @@ pub mod server_state;
 pub mod structures;
 pub mod types;
 pub mod logs;
+
+pub mod built_info {
+   // The file has been placed there by the build script.
+   include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
--- rust/src/logs.rs
+++ rust/src/logs.rs
@@ -1,4 +1,5 @@
 use std::env;
+use crate::built_info;
 
 fn get_exec_name() -> Option<String> {
     std::env::current_exe()
@@ -7,9 +8,12 @@ fn get_exec_name() -> Option<String> {
         .and_then(|s| s.into_string().ok())
 }
 
+// use built_info::*;
+
 pub static DEFAULT_LOG_LEVEL: &'static str = "warn,dtps_http=info";
 pub fn init_logging () {
 
+    let git_version = built_info::GIT_VERSION;
     let s = get_exec_name().unwrap();
     let version = env!("CARGO_PKG_VERSION");
     let pkg_name = env!("CARGO_PKG_NAME");
