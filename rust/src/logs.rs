6c760dedad6c91b9582c1ed6da3bec807faec99f
Andrea Censi <AndreaCensi@users.noreply.github.com>
2023-07-07 15:26:30 +0200

    misc

--- rust/src/logs.rs
+++ rust/src/logs.rs
@@ -1,4 +1,5 @@
 use std::env;
+
 use crate::built_info;
 
 fn get_exec_name() -> Option<String> {
@@ -11,19 +12,25 @@ fn get_exec_name() -> Option<String> {
 // use built_info::*;
 
 pub static DEFAULT_LOG_LEVEL: &'static str = "warn,dtps_http=info";
-pub fn init_logging () {
 
-    let git_version = built_info::GIT_VERSION;
-    let s = get_exec_name().unwrap();
-    let version = env!("CARGO_PKG_VERSION");
+pub fn init_logging() {
+    let git_version = built_info::GIT_VERSION.unwrap_or("unknown");
+    let git_commit_hash = built_info::GIT_COMMIT_HASH.unwrap_or("unknown");
+    let git_branch = built_info::GIT_HEAD_REF.unwrap_or("unknown");
+    let git_branch = git_branch.trim_start_matches("refs/heads/");
+    let arch = built_info::CFG_TARGET_ARCH;
+    let family = built_info::CFG_FAMILY;
+    let os = built_info::CFG_OS;
+    let profile = built_info::PROFILE ;
+    // let s = get_exec_name().unwrap();
+    // let version = env!("CARGO_PKG_VERSION");
     let pkg_name = env!("CARGO_PKG_NAME");
-    eprintln!("{s} ({pkg_name} {version}) ");
+    eprintln!("{pkg_name} {git_version}  {family}/{os} {arch} ({git_branch} {git_commit_hash} {profile})");
     if env::var("RUST_LOG").is_err() {
         eprintln!("RUST_LOG not set: setting it to {DEFAULT_LOG_LEVEL}");
         env::set_var("RUST_LOG", DEFAULT_LOG_LEVEL)
-    }else {
+    } else {
         eprintln!("RUST_LOG is set to {:?}", env::var("RUST_LOG"));
     }
     env_logger::init();
-
 }
